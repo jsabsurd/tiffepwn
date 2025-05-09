d997ec339f17c901e3e235d34919b91cd4144528
Johnny Stauch <joh.stauch@proton.me>
2025-05-09 18:32:37 +0200

    fix: outsource menu from main.py into own menu module (fixes #15)

--- modules/menu.py
+++ modules/menu.py
@@ -1,3 +1,6 @@
+from modules.os_det import clear_screen
+from time import sleep
+
 def show_menu():
     print("select what to do:")
     print("1: nmap (simple nmap implementation)")
@@ -6,3 +9,16 @@ def show_menu():
     
     choice = input("> ")
     return choice
+
+def menu_selection(menu_choice):
+    menu_error = False
+    if menu_choice == "1":
+        print("nmap loading...")
+        sleep(2)
+    elif menu_choice == "x":
+        print("goodby!")
+        exit()
+    else:
+        menu_error=True
+    clear_screen()
+    return menu_error
